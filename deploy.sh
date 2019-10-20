7e0b0d9ae9208fd86cc0de9498a3149659e5ed50
hins <619684051@qq.com>
2019-10-20 13:44:13 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ commit_statement=$commit_statement$1
 
 git add *
 git commit -m "$commit_statement"
-git push https://619684051@qq.com:ab19970326..@github.com/Owen-Liuyuxuan/papers_reading_sharing.github.io.git
+git push https://yuxuan:yuxuan123@git.ram-lab.com/yuxuan/My_papers.git
+git push https://Owen-Liuyuxuan:ab619684051@qq.com@github.com/Owen-Liuyuxuan/papers_reading_sharing.github.io.git
