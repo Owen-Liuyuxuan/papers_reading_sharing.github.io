1a289abe58ba287ccc9338509ca7c5b3b7d82495
owenliu <619684051@qq.com>
2019-10-24 23:08:42 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 commit_statement="add "
 commit_statement=$commit_statement$1
 
-
+echo "git pull"
+git pull
+echo "git add *"
 git add *
+echo "git commit"
 git commit -m "$commit_statement"
+echo "git push"
 git push https://github.com/Owen-Liuyuxuan/papers_reading_sharing.github.io.git
-
+echo "mkdocs gh-deploy"
 mkdocs gh-deploy
